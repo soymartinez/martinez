13bfb0cf96d47f2d7ac878d147aa2986e20545e3
omgismartinez <martnzomg@gmail.com>
2022-07-01 22:42:11 -0500

    feat: create navbar

---
+++ components/navbar.js
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function Nabvar() {
+
+    const [scroll, setScroll] = useState(false)
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScroll(true)
+            } else {
+                setScroll(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+    }, []);
+
+    const router = useRouter()
+    const nav = [
+        { name: 'Home', href: '/' },
+        { name: 'About', href: '/about' },
+        { name: 'Projects', href: '/projects' },
+        { name: 'Contact', href: '/contact' },
+    ]
+
+    return (
+        <div className='sticky top-0 z-10 '>
+            <div className={`
+                flex justify-center items-center 
+                backdrop-blur-sm ${scroll ? 'border-b' : 'border-none'} border-zinc-900            
+                absolute transition-all
+                w-full h-16`}>
+                {
+                    nav.map(item => (
+                        <Link href={item.href} key={item.name}>
+                            <a className={`block px-4 py-2 hover:text-white font-semibold text-sm
+                                ${router.asPath == item.href ? 'text-white' : ''}`}>
+                                {item.name}
+                            </a>
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
