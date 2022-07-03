41b4094940d1263de4971276062fcce8781219ae
omgismartinez <martnzomg@gmail.com>
2022-07-02 23:02:21 -0500

    refactor (navbar): menu, 1/2 responsive

--- components/navbar.js
+++ components/navbar.js
@@ -27,20 +27,20 @@ export default function Nabvar() {
     ]
 
     const menu = (
-        <div className={`w-6 space-y-1 md:hidden ${isActive ? '-space-y-0.5' : ''}`} onClick={() => setIsActive(!isActive)}>
-            <div className={`w-6 h-0.5 bg-white transition-all rounded-full ${isActive ? 'rotate-45' : 'ml-2'}`}></div>
-            <div className={`w-6 h-0.5 bg-white transition-all rounded-full ${isActive ? '-rotate-45' : ''}`}></div>
+        <div className={`space-y-1 md:hidden ${isActive ? '-space-y-0.5' : ''}`} onClick={() => setIsActive(!isActive)}>
+            <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? 'rotate-45' : 'ml-2'}`}></div>
+            <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? '-rotate-45' : ''}`}></div>
         </div>
     )
 
     const mode = (
-        <div className='md:hidden'>Dark mode</div>
+        <div className=''>Dark mode</div>
     )
 
     const navbar = (
         nav.map(nav => (
             <Link href={nav.href} key={nav.name}>
-                <a className={`px-4 py-2 hover:text-white font-semibold text-sm hidden md:flex
+                <a className={`px-4 py-2 hover:text-white font-semibold text-sm hidden md:block
                             ${router.asPath == nav.href ? 'text-white' : ''}`}>
                     {nav.name}
                 </a>
@@ -49,7 +49,7 @@ export default function Nabvar() {
     )
 
     const navbarMobile = (
-        <div className={`flex flex-col items-center justify-center 
+        <div className={`flex flex-col 
                         ${isActive ? 'visible' : 'hidden'}
                         md:hidden
                         absolute mt-16 top-0`}>
@@ -59,17 +59,17 @@ export default function Nabvar() {
 
     return (
         <div className='sticky top-0 z-10
-                        md:max-w-3xl mx-auto'>
+                        sm:max-w-3xl mx-auto'>
             <div className={`
-                flex items-center justify-between md:justify-center
+                flex items-center justify-between md:justify-start
                 backdrop-blur-sm border-b-2 ${scroll ? 'border-zinc-900' : 'border-transparent'}            
                 absolute transition-all
                 w-full h-16 
                 px-6`}>
                 {menu}
-                {mode}
                 {navbar}
                 {navbarMobile}
+                {mode}
             </div>
         </div>
     )
