ed85848fe9f4fd9d56d8cd762272dcb957869a9a
omgismartinez <martnzomg@gmail.com>
2022-08-27 23:08:48 -0500

    🌌 refactor: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './components/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#6241ea',
+        'dark': '#040407',
+        'white': '#ffffff',
+        'neutral': '#e5e5e5',
+        'stone': '#888888',
+        'gray': '#16161a',
+        'green': '#1ed760',
+      },
+    },
   },
   plugins: [],
 }
