0d87b706e5fc7c1ab70009ce77e65e1e2ae4b93c
omgismartinez <martnzomg@gmail.com>
2022-08-31 23:42:59 -0500

    🌌 refactor (header): orthography

--- components/header.js
+++ components/header.js
@@ -12,8 +12,8 @@ export default function Header() {
                         Frontend Developer, JS Developer.
                     </span>
                     <span className='mt-3'>
-                        I building web apps with React / Next.js and Tailwind CSS and more.
-                        {`I'm a JavaScript enthusiast and I love doing design and UI.`}
+                        {`I building web apps with React / Next.js and Tailwind CSS and more.
+                        I'm a JavaScript enthusiast and I love doing design and UI.`}
                     </span>
                 </div>
                 <div className='my-6 sm:my-0 sm:w-[170px] w-[80px]'>
