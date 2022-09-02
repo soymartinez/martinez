c582ee228705881679aa8d776b6114a85faa15bb
omgismartinez <martnzomg@gmail.com>
2022-09-01 23:40:50 -0500

    🌌 refactor (layout): background animation

--- components/layout.js
+++ components/layout.js
@@ -4,15 +4,21 @@ import Nabvar from './navbar'
 export default function Layout({ title, children }) {
     return (
         <div>
+            <div className='relative '>
+                <div className='absolute left-10 top-80 rounded-full w-64 h-80 bg-primary opacity-40 animate-smooth bg-blend-multiply' />
+                <div className='absolute left-52 top-64 rounded-full w-64 h-80 bg-sky-500 opacity-60 animate-smooth bg-blend-multiply' />
+                <div className='absolute -left-10 top-96 rounded-full w-64 h-80 bg-green opacity-80 animate-smooth bg-blend-multiply' />
+                <div className='absolute -left-96 top-80 rounded-full w-64 h-80 bg-white opacity-70 animate-smooth bg-blend-multiply' />
+            </div>
             <Head>
                 <title>{title}</title>
                 <meta name="description" content="Frontend developer, JS developer" />
                 <meta name="twitter:site" content="@omgismartinez" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            
+
             <Nabvar />
-            <main className='relative px-6 w-screen h-screen'>{children}</main>
+            <main className='relative px-6 w-screen h-screen backdrop-blur-2xl'>{children}</main>
         </div>
     )
 }
