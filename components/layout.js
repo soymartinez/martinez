33b80883c7a8c97921a1e158f43b8818ea9f6ec4
omgismartinez <martnzomg@gmail.com>
2022-09-02 23:16:31 -0500

    🌌 refactor (layout): overflow-hidden

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import Nabvar from './navbar'
 
 export default function Layout({ title, children }) {
     return (
-        <div>
-            <div className='relative '>
+        <div className='overflow-hidden'>
+            <div className='relative'>
                 <div className='absolute left-10 top-80 rounded-full w-64 h-80 bg-primary opacity-40 animate-smooth bg-blend-multiply' />
                 <div className='absolute left-52 top-64 rounded-full w-64 h-80 bg-sky-500 opacity-60 animate-smooth bg-blend-multiply' />
                 <div className='absolute -left-10 top-96 rounded-full w-64 h-80 bg-green opacity-80 animate-smooth bg-blend-multiply' />
