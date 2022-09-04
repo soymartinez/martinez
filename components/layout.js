3c9861e0fb95cd882bc12c238a7613e0880b07fe
omgismartinez <martnzomg@gmail.com>
2022-09-03 23:53:42 -0500

    🌌 refactor (layout): animation

--- components/layout.js
+++ components/layout.js
@@ -5,10 +5,7 @@ export default function Layout({ title, children }) {
     return (
         <div className='overflow-hidden'>
             <div className='relative'>
-                <div className='absolute left-10 top-80 rounded-full w-64 h-80 bg-primary opacity-40 animate-smooth bg-blend-multiply' />
-                <div className='absolute left-52 top-64 rounded-full w-64 h-80 bg-sky-500 opacity-60 animate-smooth bg-blend-multiply' />
-                <div className='absolute -left-10 top-96 rounded-full w-64 h-80 bg-green opacity-80 animate-smooth bg-blend-multiply' />
-                <div className='absolute -left-96 top-80 rounded-full w-64 h-80 bg-white opacity-70 animate-smooth bg-blend-multiply' />
+                <div className='absolute rounded-full w-96 h-96 bg-primary animate-smooth bg-blend-multiply' />
             </div>
             <Head>
                 <title>{title}</title>
@@ -18,7 +15,7 @@ export default function Layout({ title, children }) {
             </Head>
 
             <Nabvar />
-            <main className='relative px-6 w-screen h-screen backdrop-blur-2xl'>{children}</main>
+            <main className='relative px-6 w-screen h-screen backdrop-blur-3xl'>{children}</main>
         </div>
     )
 }
