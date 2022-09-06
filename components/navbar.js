c3e4facd6ebf22397edcbdbfbdd8baf62f1c117b
omgismartinez <martnzomg@gmail.com>
2022-09-05 23:24:22 -0500

    🌌 refactor (navbar): design

--- components/navbar.js
+++ components/navbar.js
@@ -26,13 +26,6 @@ export default function Nabvar() {
         { name: 'Contact', href: '/contact' },
     ]
 
-    const menu = (
-        <div className={`space-y-1 px-4 py-6 -ml-4 md:hidden ${isActive ? '-space-y-0.5' : ''}`} onClick={() => setIsActive(!isActive)}>
-            <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? 'rotate-45' : 'ml-2'}`}></div>
-            <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? '-rotate-45' : ''}`}></div>
-        </div>
-    )
-
     const mode = (
         <div className=''>Dark mode</div>
     )
@@ -40,7 +33,7 @@ export default function Nabvar() {
     const navLinks = (
         nav.map(nav => (
             <Link href={nav.href} key={nav.name}>
-                <a className={`md:px-4 py-2 hover:text-white font-semibold
+                <a className={`md:px-4 py-2 hover:text-white font-semibold hover:translate-x-1 transition-all
                             ${router.asPath == nav.href ? 'text-white' : ''}`}>
                     {nav.name}
                 </a>
@@ -48,31 +41,24 @@ export default function Nabvar() {
         ))
     )
 
-    const navbar = (
-        <div className='hidden md:block md:-ml-4 text-sm'>
-            {navLinks}
-        </div>
-    )
-
-    const navbarMobile = (
-        <div className={`flex flex-col w-full h-screen
-                        ${isActive ? 'opacity-100' : 'hidden'}
-                        md:hidden left-0 transition-all text-xl
-                        bg-gradient-to-b from-[#040407] to-black
-                        absolute mt-20 top-0 space-y-4 px-6`}>
-            {navLinks}
-        </div>
-    )
-
     return (
         <div className={`fixed top-0 z-50 w-full px-6
-                backdrop-blur-sm border-b-2 transition-all 
+                backdrop-blur-sm border-b-2 transition-all
                 ${scroll ? 'border-zinc-900' : 'border-transparent'}`}>
-            <div className={`md:max-w-3xl mx-auto
-                flex items-center justify-between h-20`}>
-                {menu}
-                {navbar}
-                {navbarMobile}
+            <div className={`md:max-w-3xl mx-auto flex items-center justify-between h-20 relative`}>
+                <div className='hidden md:block md:-ml-4 text-sm'>
+                    {navLinks}
+                </div>
+                <div>
+                    <div className={`space-y-1 px-4 py-6 -ml-4 md:hidden ${isActive ? '-space-y-0.5' : ''}`} onClick={() => setIsActive(!isActive)}>
+                        <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? 'rotate-45' : 'ml-2'}`}></div>
+                        <div className={`w-5 h-0.5 bg-white transition-all rounded-full ${isActive ? '-rotate-45' : ''}`}></div>
+                    </div>
+                    <div className={`flex flex-col absolute w-full top-20 p-6 bg-dark/60 backdrop-blur-sm rounded-xl 
+                        space-y-4 text-lg transition-all ${isActive ? 'opacity-100' : 'hidden'}`}>
+                        {navLinks}
+                    </div>
+                </div>
                 {mode}
             </div>
         </div>
