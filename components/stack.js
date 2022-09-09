bb0d6fd905d959292471e60b947d7069c099fc3d
omgismartinez <martnzomg@gmail.com>
2022-09-08 23:51:03 -0500

    🚀 feat: stack

---
+++ components/stack.js
@@ -0,0 +1,84 @@
+export default function Stack() {
+    const stack = [
+        {
+            name: 'React',
+            icon: '/icons/react.svg',
+            url: 'https://reactjs.org/',
+        },
+        {
+            name: 'Next.js',
+            icon: '/icons/nextjs.svg',
+            url: 'https://nextjs.org/',
+        },
+        {
+            name: 'Tailwind CSS',
+            icon: '/icons/tailwindcss.svg',
+            url: 'https://tailwindcss.com/',
+        },
+        {
+            name: 'Node.js',
+            icon: '/icons/nodejs.svg',
+            url: 'https://nodejs.org/',
+        },
+        {
+            name: 'MongoDB',
+            icon: '/icons/mongodb.svg',
+            url: 'https://www.mongodb.com/',
+        },
+        {
+            name: 'PostgreSQL',
+            icon: '/icons/postgresql.svg',
+            url: 'https://www.postgresql.org/',
+        },
+        {
+            name: 'Git',
+            icon: '/icons/git.svg',
+            url: 'https://git-scm.com/',
+        },
+        {
+            name: 'GitHub',
+            icon: '/icons/github.svg',
+            url: '',
+        },
+        {
+            name: 'Vercel',
+            icon: '/icons/vercel.svg',
+            url: 'https://vercel.com/',
+        },
+        {
+            name: 'Figma',
+            icon: '/icons/figma.svg',
+            url: 'https://www.figma.com/',
+        },
+        {
+            name: 'Prisma',
+            icon: '/icons/prisma.svg',
+            url: 'https://www.prisma.io/',
+        },
+        {
+            name: 'JavaScript',
+            icon: '/icons/javascript.svg',
+            url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        },
+        {
+            name: 'TypeScript',
+            icon: '/icons/typescript.svg',
+            url: 'https://www.typescriptlang.org/',
+        },
+    ]
+
+    return (
+        <div className='backdrop-blur-sm'>
+            <h1 className='text-white'>Technologies</h1>
+            <div className='flex flex-wrap justify-center'>
+                {
+                    stack.map((tech) => (
+                        <div className='flex flex-col items-center m-4' key={tech.name}>
+                            <p>{tech.name}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
