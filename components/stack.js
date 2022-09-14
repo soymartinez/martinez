ec302bff249b3ff6ba814ee74b9988b1473f9b71
omgismartinez <martnzomg@gmail.com>
2022-09-13 23:47:37 -0500

    🌌 refactor (stack): type of technology

--- components/stack.js
+++ components/stack.js
@@ -1,69 +1,131 @@
 export default function Stack() {
     const stack = [
         {
-            name: 'React',
-            icon: '/icons/react.svg',
-            url: 'https://reactjs.org/',
-        },
-        {
-            name: 'Next.js',
-            icon: '/icons/nextjs.svg',
-            url: 'https://nextjs.org/',
-        },
-        {
-            name: 'Tailwind CSS',
-            icon: '/icons/tailwindcss.svg',
-            url: 'https://tailwindcss.com/',
-        },
-        {
-            name: 'Node.js',
-            icon: '/icons/nodejs.svg',
-            url: 'https://nodejs.org/',
-        },
-        {
-            name: 'MongoDB',
-            icon: '/icons/mongodb.svg',
-            url: 'https://www.mongodb.com/',
-        },
-        {
-            name: 'PostgreSQL',
-            icon: '/icons/postgresql.svg',
-            url: 'https://www.postgresql.org/',
-        },
-        {
-            name: 'Git',
-            icon: '/icons/git.svg',
-            url: 'https://git-scm.com/',
-        },
-        {
-            name: 'GitHub',
-            icon: '/icons/github.svg',
-            url: '',
-        },
-        {
-            name: 'Vercel',
-            icon: '/icons/vercel.svg',
-            url: 'https://vercel.com/',
-        },
-        {
-            name: 'Figma',
-            icon: '/icons/figma.svg',
-            url: 'https://www.figma.com/',
-        },
-        {
-            name: 'Prisma',
-            icon: '/icons/prisma.svg',
-            url: 'https://www.prisma.io/',
-        },
-        {
-            name: 'JavaScript',
-            icon: '/icons/javascript.svg',
-            url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-        },
-        {
-            name: 'TypeScript',
-            icon: '/icons/typescript.svg',
-            url: 'https://www.typescriptlang.org/',
+            "frontend": [
+                {
+                    name: 'React',
+                    icon: '/icons/react.svg',
+                    url: 'https://reactjs.org/',
+                },
+                {
+                    name: 'Next.js',
+                    icon: '/icons/nextjs.svg',
+                    url: 'https://nextjs.org/',
+                },
+                {
+                    name: 'Tailwind CSS',
+                    icon: '/icons/tailwindcss.svg',
+                    url: 'https://tailwindcss.com/',
+                },
+                {
+                    name: 'Framer Motion',
+                    icon: '/icons/framer-motion.svg',
+                    url: 'https://www.framer.com/motion/',
+                },
+                {
+                    name: 'HTML',
+                    icon: '/icons/html.svg',
+                    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+                },
+                {
+                    name: 'CSS',
+                    icon: '/icons/css.svg',
+                    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+                },
+            ],
+            "backend": [
+                {
+                    name: 'Node.js',
+                    icon: '/icons/nodejs.svg',
+                    url: 'https://nodejs.org/en/',
+                },
+                {
+                    name: 'Express.js',
+                    icon: '/icons/expressjs.svg',
+                    url: 'https://expressjs.com/',
+                },
+                {
+                    name: 'Prisma',
+                    icon: '/icons/prisma.svg',
+                    url: 'https://www.prisma.io/',
+                },
+                {
+                    name: 'MongoDB',
+                    icon: '/icons/mongodb.svg',
+                    url: 'https://www.mongodb.com/',
+                },
+                {
+                    name: 'PostgreSQL',
+                    icon: '/icons/postgresql.svg',
+                    url: 'https://www.postgresql.org/',
+                },
+                {
+                    name: 'Supabase',
+                    icon: '/icons/supabase.svg',
+                    url: 'https://supabase.io/',
+                },
+                {
+                    name: 'Firebase',
+                    icon: '/icons/firebase.svg',
+                    url: 'https://firebase.google.com/',
+                },
+            ],
+            "tools": [
+                {
+                    name: 'VS Code',
+                    icon: '/icons/vscode.svg',
+                    url: 'https://code.visualstudio.com/',
+                },
+                {
+                    name: 'Git',
+                    icon: '/icons/git.svg',
+                    url: 'https://git-scm.com/',
+                },
+                {
+                    name: 'GitHub',
+                    icon: '/icons/github.svg',
+                    url: 'https://github.com/'
+                },
+                {
+                    name: 'Figma',
+                    icon: '/icons/figma.svg',
+                    url: 'https://www.figma.com/',
+                },
+                {
+                    name: 'Framer',
+                    icon: '/icons/framer.svg',
+                    url: 'https://www.framer.com/',
+                }
+            ],
+            "misc": [
+                {
+                    name: 'Vercel',
+                    icon: '/icons/vercel.svg',
+                    url: 'https://vercel.com/',
+                },
+            ],
+            "learning": [
+                {
+                    name: 'Deno',
+                    icon: '/icons/deno.svg',
+                    url: 'https://deno.land/',
+                },
+                {
+                    name: 'Fresh',
+                    icon: '/icons/fresh.svg',
+                    url: 'https://fresh.deno.dev/',
+                },
+                {
+                    name: 'Astro',
+                    icon: '/icons/astro.svg',
+                    url: 'https://astro.build/',
+                },
+                {
+                    name: 'GraphQL',
+                    icon: '/icons/graphql.svg',
+                    url: 'https://graphql.org/',
+                },
+            ],
         },
     ]
 
