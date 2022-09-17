2a2210d439f8bd72c4c20f3e49bc20daaf0b1282
omgismartinez <martnzomg@gmail.com>
2022-09-16 19:47:42 -0500

    🚀 feat: css component

---
+++ components/icons/css.js
@@ -0,0 +1,14 @@
+export default function CSS() {
+    return (
+        <svg
+            width={24}
+            height={24}
+            fill='currentColor'
+            viewBox='0 0 24 24'
+            className='w-8 h-auto md:w-10 justify-self-center text-[#254bdd]'
+        >
+            <title>{'CSS3'}</title>
+            <path d='M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z' />
+        </svg>
+    )
+}
