b86c71fce72222a5d5b9ff5618681cc96afc004c
omgismartinez <martnzomg@gmail.com>
2022-09-17 19:01:45 -0500

    🌌 refactor (proyects): section design

--- components/proyects.js
+++ components/proyects.js
@@ -14,31 +14,33 @@ const proyects = [
 
 export default function Proyects() {
     return (
-        <div className='grid md:grid-cols-2 gap-4 my-4'>
+        <section className='flex flex-col gap-6'>
             <h1 className='text-xl text-white font-bold'>Proyects</h1>
-            {
-                proyects.map(proyect => (
-                    <section key={proyect.title} className={'overflow-hidden relative rounded-xl'}>
-                        <div className='flex flex-col backdrop-blur-3xl overflow-hidden'>
-                            <div className='flex items-center justify-between py-4 px-6'>
-                                <div className='truncate'>
-                                    <a href={proyect.url} target='_blank' rel='noreferrer'>
-                                        <h1 className='text-white font-semibold text-xl z-20 hover:underline'>{proyect.title}</h1>
+            <div className='grid md:grid-cols-2 gap-4'>
+                {
+                    proyects.map(proyect => (
+                        <section key={proyect.title} className={'overflow-hidden relative rounded-xl'}>
+                            <div className='flex flex-col backdrop-blur-3xl overflow-hidden'>
+                                <div className='flex items-center justify-between py-4 px-6'>
+                                    <div className='truncate'>
+                                        <a href={proyect.url} target='_blank' rel='noreferrer'>
+                                            <h1 className='text-white font-semibold text-xl z-20 hover:underline'>{proyect.title}</h1>
+                                        </a>
+                                        <p className='text-stone text-sm'>{proyect.description}</p>
+                                    </div>
+                                    <a href={proyect.github} target='_blank' rel='noreferrer'>
+                                        <Github />
                                     </a>
-                                    <p className='text-stone text-sm'>{proyect.description}</p>
                                 </div>
-                                <a href={proyect.github} target='_blank' rel='noreferrer'>
-                                    <Github />
-                                </a>
+                                <div className='shadow-md'>
+                                    <Image src={proyect.image} priority={true}
+                                        className='rounded-xl' alt={proyect.title} width={400} height={200} layout='responsive' objectFit='contain' />
+                                </div>
                             </div>
-                            <div className='shadow-md'>
-                                <Image src={proyect.image} priority={true}
-                                    className='rounded-xl' alt={proyect.title} width={400} height={200} layout='responsive' objectFit='contain' />
-                            </div>
-                        </div>
-                    </section>
-                ))
-            }
-        </div>
+                        </section>
+                    ))
+                }
+            </div>
+        </section>
     )
 }
