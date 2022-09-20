ee616dc33b3105ea52248882d285fa051629a5bb
omgismartinez <martnzomg@gmail.com>
2022-09-19 22:36:08 -0500

    🚀 feat: figma component

---
+++ components/icons/figma.js
@@ -0,0 +1,20 @@
+export default function Figma() {
+    return (
+        <svg
+            width={24}
+            height={24}
+            fill='currentColor'
+            viewBox='0 0 24 24'
+            className='w-8 h-auto md:w-10 justify-self-start'
+        >
+            <g fill="none" fillRule="evenodd" transform="translate(4)">
+                <circle cx={12} cy={12} r={4} fill="#19BCFE" />
+                <title>{'Figma'}</title>
+                <path fill="#09CF83" d="M4 24a4 4 0 0 0 4-4v-4H4a4 4 0 1 0 0 8Z" />
+                <path fill="#A259FF" d="M4 16h4V8H4a4 4 0 1 0 0 8Z" />
+                <path fill="#F24E1E" d="M4 8h4V0H4a4 4 0 1 0 0 8Z" />
+                <path fill="#FF7262" d="M12 8H8V0h4a4 4 0 1 1 0 8Z" />
+            </g>
+        </svg>
+    )
+}
