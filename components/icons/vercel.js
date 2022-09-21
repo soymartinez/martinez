891c86d7b6afce96d0ead40aed03c04871a4cd31
omgismartinez <martnzomg@gmail.com>
2022-09-21 17:05:41 -0500

    feat: vercel component

---
+++ components/icons/vercel.js
@@ -0,0 +1,14 @@
+export default function Vercel() {
+    return (
+        <svg
+            width={24}
+            height={24}
+            fill='currentColor'
+            viewBox='0 0 512 512'
+            className='w-8 h-auto md:w-10 justify-self-center text-white'
+        >
+            <title>{'Vercel'}</title>
+            <path fillRule='evenodd' d='m256 48 240 416H16Z' />
+        </svg>
+    )
+}
