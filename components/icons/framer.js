00fe5e86421bac5bf7aea7393216813b4448711b
omgismartinez <martnzomg@gmail.com>
2022-09-20 20:24:33 -0500

    🚀 feat: framer component

---
+++ components/icons/framer.js
@@ -0,0 +1,14 @@
+export default function Framer() {
+    return (
+        <svg
+            width={24}
+            height={24}
+            fill='currentColor'
+            viewBox='0 0 24 24'
+            className='w-8 h-auto md:w-10 justify-self-start text-dark dark:text-white'
+        >
+            <title>{'Framer'}</title>
+            <path d='M4 0h16v8h-8zm0 8h8l8 8H4zm0 8h8v8z' />
+        </svg>
+    )
+}
