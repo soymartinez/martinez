1e498ab329870baf64e9b9d0d346dce1c95c66cf
omgismartinez <martnzomg@gmail.com>
2022-10-08 23:04:21 -0500

    🚀 feat: about

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Layout from 'components/layout'
+
+export default function About() {
+    return (
+        <Layout title={'About – Martinez'}>
+            <div className='flex flex-col gap-8 sm:max-w-3xl mx-auto'>
+                <h1>About Me</h1>
+                <section>
+                    <h1>Links</h1>
+                    <ul>
+                        <li>Twitter: <a href='https://twitter.com/omgismartinez'>@omgismartinez</a></li>
+                        <li>GitHub: <a href='https://github.com/omgismartinez'>@omgismartinez</a></li>
+                        <li>Website: <a href='https://martinez.vercel.app'>@omgismartinez</a></li>
+                        <li>LinkedIn: <a href='https://linkedin.com/in/omgismartinez'>@omgismartinez</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h1>Bio</h1>
+                    <div>
+                        <h2>Education</h2>
+                        <p>Alvaro Martinez studying Software Engineering at the Universidad Veracruzana.</p>
+                        {/* <p>Alvaro Martinez graduated from the Universidad Veracruzana with a degree in Software Engineering.</p> */}
+                    </div>
+                </section>
+            </div>
+        </Layout>
+    )
+}
