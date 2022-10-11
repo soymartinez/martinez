597580b06e55b0e073f7822cbec80bd7e6e214e9
omgismartinez <martnzomg@gmail.com>
2022-10-10 23:41:19 -0500

    🚀 feat: projects

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import Layout from 'components/layout'
+
+export default function Projects() {
+  return (
+    <Layout title={'Projects – Martinez'}>
+      <div className='flex flex-col gap-8 sm:max-w-3xl mx-auto'>
+        <h1 className='text-white text-3xl md:text-5xl font-bold my-6 sm:my-0'>Projects</h1>
+      </div>
+    </Layout>
+  )
+}
