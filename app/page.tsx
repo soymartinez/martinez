2f4fdc72bf828706b84f6c08bf2b80ddb9afbe12
omgismartinez <martnzomg@gmail.com>
2023-02-13 23:57:42 -0600

    🌌 refactor (home): landing page  components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Header from 'components/header'
+import Proyects from 'components/proyects'
+import Stack from 'components/stack'
+import Spotify from 'components/spotify'
+
 export default function Home() {
     return (
-        <main>
-            Alvaro Martinez
-        </main>
+        <main className='flex flex-col gap-8 sm:max-w-3xl mx-auto'>
+            <Header />
+            <Proyects />
+            <Stack />
+            <Spotify />
+        </main >
     )
 }
