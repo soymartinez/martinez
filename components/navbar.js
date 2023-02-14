e16f1f26ce594766bc6bfb9d92e030f95689b01a
omgismartinez <martnzomg@gmail.com>
2023-02-14 01:12:34 -0600

    🌌 refactor (navbar): useRouter from navigation + use client

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,7 @@
+'use client'
+
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function Nabvar() {
