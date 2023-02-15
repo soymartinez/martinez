297ec335d6160992ed4918137a885d8f3f4b33ff
omgismartinez <martnzomg@gmail.com>
2023-02-14 23:16:31 -0600

    🌌 refactor (layout): navbar + top padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Familjen_Grotesk as Font } from '@next/font/google'
+import Navbar from 'ui/navbar'
 
 const font = Font({
   subsets: ['latin'],
@@ -15,9 +16,10 @@ export default function RootLayout({
     <html lang='en' className='dark' style={{ colorScheme: 'dark' }}>
       <head />
       <body className={`dark:bg-dark ${font.variable} font-sans`}>
+        <Navbar />
         <div className='relative overflow-hidden'>
           <div className='absolute rounded-full w-96 h-96 blur-3xl bg-primary animate-smooth md:animate-smooth2' />
-          <main className='relative px-6 py-20'>
+          <main className='relative px-6 py-20 sm:pt-32'>
             {children}
           </main>
         </div>
