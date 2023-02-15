2df25423af1002e921547b7e80bdfbd4a7cd00b9
omgismartinez <martnzomg@gmail.com>
2023-02-15 01:31:17 -0600

    🚀 feat: blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default async function Blog() {
+    return (
+        <main className='sm:max-w-3xl mx-auto'>
+            <h1 className='text-xl font-bold'>Blog</h1>
+        </main>
+    )
+}
