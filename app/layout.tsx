76ccd14326c6b6af324aa7a94376e1db11f5a1d1
omgismartinez <martnzomg@gmail.com>
2023-02-18 01:06:27 -0600

    🌌 refactor (layout): application wrapped in the theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { Familjen_Grotesk as Font } from '@next/font/google'
 import Navbar from 'ui/navbar'
+import { Providers } from 'ui/providers'
 
 const font = Font({
   subsets: ['latin'],
@@ -13,16 +14,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className='dark' style={{ colorScheme: 'dark' }}>
+    <html lang='en'>
       <head />
       <body className={`dark:bg-dark ${font.variable} font-sans`}>
-        <Navbar />
-        <div className='relative overflow-hidden'>
-          <div className='absolute rounded-full w-96 h-96 blur-3xl bg-primary animate-smooth md:animate-smooth2' />
-          <main className='relative px-6 py-20 sm:pt-32'>
-            {children}
-          </main>
-        </div>
+        <Providers>
+          <Navbar />
+          <div className='relative overflow-hidden'>
+            <div className='absolute rounded-full w-96 h-96 blur-3xl bg-primary animate-smooth md:animate-smooth2' />
+            <main className='relative px-6 py-20 sm:pt-32'>
+              {children}
+            </main>
+          </div>
+        </Providers>
       </body>
     </html>
   )
