5c3f6f6dda126bec233c9c0920516b798354e181
omgismartinez <martnzomg@gmail.com>
2023-02-18 12:42:32 -0600

    🌌 refactor (layout): light background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head />
-      <body className={`dark:bg-dark ${font.variable} font-sans`}>
+      <body className={`bg-light dark:bg-dark ${font.variable} font-sans`}>
         <Providers>
           <Navbar />
           <div className='relative overflow-hidden'>
