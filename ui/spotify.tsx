5c23d289e1a3c03c6b2ccd295fabb11a0ed22231
omgismartinez <martnzomg@gmail.com>
2023-02-18 14:21:37 -0600

    🌌 refactor (spotify): supports dark mode + responsive

--- ui/spotify.tsx
+++ ui/spotify.tsx
@@ -19,16 +19,16 @@ export default function Spotify() {
 
     if (!data) return (
         <div className='flex justify-start items-center gap-2'>
-            <div className='w-[25px] h-[25px] rounded-full bg-zinc-800 animate-pulse' />
-            <span className='w-[5.4rem] h-4 rounded-full bg-zinc-700 animate-pulse' />
-            <span className='w-[4.4rem] h-4 rounded-full bg-zinc-900 animate-pulse' />
+            <div className='w-[25px] h-[25px] rounded-full bg-dark/40 dark:bg-light/40 animate-pulse' />
+            <span className='w-[5.4rem] h-4 rounded-full bg-dark/20 dark:bg-light/20 animate-pulse' />
+            <span className='w-[4.4rem] h-4 rounded-full bg-dark/10 dark:bg-light/10 animate-pulse' />
         </div>
     )
 
     return (
         <div className='flex justify-start items-center gap-2'>
             {data.albumImageUrl ?
-                <div className='flex gap-2 overflow-hidden'>
+                <div className='flex gap-2 truncate'>
                     <div className='flex justify-center items-center'>
                         <Image
                             src={data.albumImageUrl}
@@ -36,16 +36,13 @@ export default function Spotify() {
                             width={25}
                             height={25}
                             priority
-                            className='rounded-full z-20'
-                            style={{
-                                maxWidth: "100%",
-                                height: "auto"
-                            }} />
+                            className='rounded-full z-20 object-contain'
+                        />
                         <div className='w-5 h-5 rounded-full bg-green animate-ping absolute z-10' />
                     </div>
-                    <strong className='text-dark dark:text-white font-semibold whitespace-nowrap'>{data.title}</strong>
+                    <strong className='text-dark dark:text-white font-semibold truncate'>{data.title}</strong>
                     –
-                    <strong className='font-medium whitespace-nowrap'>{data.artist}</strong>
+                    <strong className='font-medium truncate'>{data.artist}</strong>
                 </div>
                 : notPlaying
             }
