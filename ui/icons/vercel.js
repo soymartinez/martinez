29125d55c2f0610568a25e65e7c543b1eab41b8b
omgismartinez <martnzomg@gmail.com>
2023-02-18 12:55:11 -0600

    🌌 refactor: supports dark mode

--- ui/icons/vercel.js
+++ ui/icons/vercel.js
@@ -5,7 +5,7 @@ export default function Vercel() {
             height={24}
             fill='currentColor'
             viewBox='0 0 512 512'
-            className='w-8 h-auto md:w-10 justify-self-center text-white'
+            className='w-8 h-auto md:w-10 justify-self-center text-dark dark:text-white'
         >
             <title>{'Vercel'}</title>
             <path fillRule='evenodd' d='m256 48 240 416H16Z' />
