9ad241f952d386a7136831913b84bd0c8f230c49
omgismartinez <martnzomg@gmail.com>
2023-02-23 00:02:49 -0600

    🌌 refactor (blog): metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Blog',
+    description: 'Read my experiences about software development, design and much more.',
+}
+
 export default async function Blog() {
     return (
         <main className='sm:max-w-3xl mx-auto'>
