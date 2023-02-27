ebb44f71fe590e7070ecbd164aa15ef09d0592d7
omgismartinez <martnzomg@gmail.com>
2023-02-26 23:37:08 -0600

    🚀 feat (blog): unique blog

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import Content from 'content/scroll.mdx'
+
+export default function Blog() {
+    return (
+        <main className='sm:max-w-3xl mx-auto'>
+            <h1 className='text-4xl text-white font-bold'>Real Scroll for a Chat Application</h1>
+            <Content />
+        </main>
+    )
+}
