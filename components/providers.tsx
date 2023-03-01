88268815944727f484d868655c02925888ed1b58
omgismartinez <martnzomg@gmail.com>
2023-03-01 12:44:26 -0600

    🌌 refactor (providers): logic deleted when is mounted

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 export function Providers({ children }) {
-    const [mounted, setMounted] = useState(false)
-
-    useEffect(() => setMounted(true), [])
-
-    if (!mounted) return null
-
     return <ThemeProvider attribute='class'>{children}</ThemeProvider>
 }
