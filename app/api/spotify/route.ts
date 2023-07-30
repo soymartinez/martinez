acf12a0a1b168427705e00e2bd6bd92edaaf511f
omgismartinez <martnzomg@gmail.com>
2023-07-29 18:14:40 -0600

    🌌 refactor (spotify): revalidate route

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -11,6 +11,8 @@ const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 
+export const revalidate = 0
+
 export async function GET() {
     const { access_token } = await fetch(TOKEN_ENDPOINT, {
         method: 'POST',
