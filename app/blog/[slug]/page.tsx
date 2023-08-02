0993ba6b49be20850ce06eb0fbb81577e15f1a1d
omgismartinez <martnzomg@gmail.com>
2023-08-02 00:01:38 -0600

    🌌 refactor (slug): typography + color theme

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,8 +3,14 @@ import Content from 'content/scroll.mdx'
 export default function Blog() {
     return (
         <main className='sm:max-w-3xl mx-auto'>
-            <h1 className='text-4xl text-white font-bold'>Real Scroll for a Chat Application</h1>
-            <Content />
+            <section className='flex flex-col gap-6'>
+                <h1 className='text-dark dark:text-white text-3xl md:text-5xl font-bold'>
+                    Real Scroll for a Chat Application
+                </h1>
+                <div className='gap-3 flex flex-col text-dark dark:text-white'>
+                    <Content />
+                </div>
+            </section>
         </main>
     )
 }
